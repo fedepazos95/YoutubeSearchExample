9451af61f0b467939d4d3aba0dd16949e516b628
Federico Pazos <fede.pazos95@gmail.com>
2017-08-02 11:36:04 -0300

    changes to components

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -7,7 +7,13 @@ class SearchBar extends Component {
     this.state = { term: '' };
   }
   render() {
-    return <input onChange={event => console.log(event.target.value)} />;
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value })} />
+      </div>
+    )
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyBgLmNcIAYM09YhjMSFSgSINHbWPp0q6LU';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
