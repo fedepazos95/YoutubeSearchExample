47617796e5d4f1e5edbca2d23eea346fd959f5aa
Federico Pazos <fede.pazos95@gmail.com>
2017-08-08 00:21:46 -0300

    working search

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const VideoListItem = ({video}) => {
+const VideoListItem = ({video, onVideoSelect}) => {
   const imageUrl = video.snippet.thumbnails.default.url;
   return (
-    <li className="list-group-item">
+    <li onClick={() => onVideoSelect(video)} className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
           <img className="media-object" src={imageUrl} />
@@ -12,7 +12,6 @@ const VideoListItem = ({video}) => {
           <div className="media-heading">{video.snippet.title}</div>
         </div>
       </div>
-
     </li>
   );
 };
